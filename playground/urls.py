39c00541958c01701d3a49290488f4eb34ddfbfe
Wong Elone <wongelone0908@gmail.com>
2023-03-12 22:28:29 +0800

    Practice django DefaultRouter

--- playground/urls.py
+++ playground/urls.py
@@ -1,11 +1,19 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+routers = DefaultRouter()
+routers.register('course_categories', views.CourseCategoryViewSet)
+routers.register('courses', views.CourseViewSet)
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('course_categories/', views.CourseCategoryViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('course_categories/<pk>', views.CourseCategoryViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
-    path('courses/', views.CourseViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('courses/<pk>', views.CourseViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
+    path('', include(routers.urls)),
 ]
+
+# urlpatterns = [
+#     path('', views.index, name="index"),
+#     path('course_categories/', views.CourseCategoryViewSet.as_view({'get': 'list', 'post': 'create'})),
+#     path('course_categories/<pk>', views.CourseCategoryViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
+#     path('courses/', views.CourseViewSet.as_view({'get': 'list', 'post': 'create'})),
+#     path('courses/<pk>', views.CourseViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
+# ]
